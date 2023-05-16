9287e3978f82509daf29a664ea41b9af12157ad6
Python <python@it.step>
2023-05-16 19:58:50 +0300

    Updated Lesson

--- Code/example1.py
+++ Code/example1.py
@@ -1,15 +1,42 @@
-
-
-
-
-
-
-
-
-
 # ord() chr()
-a = "a"
-b = "C"
-c = "Й"
-a_ = 1049
+# a = "a"
+# b = "C"
+#
+# a_ = 104
+
+# print(ord("b"))
+# print(chr(45))
+
+# count()
+
+text = "REPositories-RElated-TO-tHe-PYthOn-Programming language"
+text_ = "language"
+# print(text.count(" "))
+
+# print(text.startswith("Repo"))
+# print(text.endswith("age"))
+# print(text.strip("e"))
+# print(text.rstrip("\n"))
+# print(text.lstrip("R").rstrip("e"))
+# print(text.find("e"))
+# print(text_.find("d"))
+# print(text.rfind("e"))
+# print(len(text))
+# print(text_.index("d"))
+
+# split
+
+# print(text.split("-"))
+l = ['Repositories', 'related', 'to', 'the', 'Python', 'Programming language']
+# print('~'.join(l))
+
+
+# upper() lower() swapcase()
+
+# print(text.upper())
+# print(text.lower())
+# print(text.swapcase())
+
+i = input().split()
+print(i)
 
