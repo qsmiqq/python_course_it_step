a52ab32f2ad2a5f17e3e69151c15b6b016063485
Aliaksandr Dvornik <aadvornik@gmail.com>
2023-05-18 15:47:31 +0300

    Added Lesson_8

---
+++ Code/example1_Garbage_collector.py
@@ -0,0 +1,20 @@
+import sys
+import gc
+
+"""Example of garbage collector"""
+
+# print(id(lst))
+# lst = [2, 6, 8, 10]
+# print(id(lst))
+# print(sys.getrefcount(lst))
+# lst_ = lst
+# print(sys.getrefcount(lst_))
+# del lst_
+#
+# print(sys.getrefcount(lst))
+# lst.append(lst)
+# print(lst)
+# print(sys.getrefcount(lst))
+# print(lst[-1] == lst)
+
+
---
+++ Homework.md
@@ -0,0 +1,2 @@
+### Lesson 8. Boolean data type. If-statement
+#### Task 1
