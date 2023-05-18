386e439069cb93cc74775b65033caf2385dd4b5b
Python <python@it.step>
2023-05-18 19:37:58 +0300

    Update lesson

--- Code/example1_Garbage_collector.py
+++ Code/example1_Garbage_collector.py
@@ -3,18 +3,25 @@ import gc
 
 """Example of garbage collector"""
 
-# print(id(lst))
-# lst = [2, 6, 8, 10]
+# lst = [1, 2, 3]
 # print(id(lst))
 # print(sys.getrefcount(lst))
 # lst_ = lst
-# print(sys.getrefcount(lst_))
+# # print(id(lst_))
+# print(sys.getrefcount(lst))
 # del lst_
-#
 # print(sys.getrefcount(lst))
 # lst.append(lst)
-# print(lst)
-# print(sys.getrefcount(lst))
-# print(lst[-1] == lst)
+# print(lst[-1] is lst)
 
+def create():
+    lst = [1, 2, 3]
+    lst.append(lst)
 
+for i in range(10):
+    create()
+
+print("Collecting...")
+n = gc.collect()
+print("Number of unreachable references ", n)
+print("Reference left", gc.garbage)
--- Code/example2_logical_operators.py
+++ Code/example2_logical_operators.py
@@ -1,10 +1,21 @@
 a = 1
-b = None
+b = 0
+c = "text"
+d = str()
+l = [False]
 
-print(a and b)
 
-print(a or b)
+# print(bool(a))
+# print(bool(b))
+# print(bool(c))
+# print(bool(d))
+# print(bool(l))
 
-print(0 or 100 or 2000)
 
-# print(None and True or (None, False) and [False])
+# print(a and b)
+#
+# print(a or b)
+#
+# print(0 or 100 or 2000)
+
+print(None and True or (None, False) and [False])
