43440a5d483dc59b81f9c8bd9fcaf4bd69e65b8e
Aliaksandr Dvornik <aadvornik@gmail.com>
2023-05-20 16:49:33 +0300

    Added tasks for Lesson_9

---
+++ Code/example4_small_tasks.py
@@ -0,0 +1,61 @@
+"""1"""
+
+# a = 100
+# b = 50
+# c = 50
+#
+# if a == b == c:
+#     print(a)
+# else:
+#     if not b == c:
+#         print(b)
+
+"""2"""
+# a = 100
+# b = 100.01
+#
+# if a < b:
+#     print(a)
+# else:
+#     print(b)
+
+
+
+
+"""3"""
+# a = 1
+# b = 2
+# c = 3
+#
+# if a > b > c:
+#     print(a)
+# else:
+#     if a > b:
+#         print(a)
+#     else:
+#         print(b)
+
+"""4"""
+
+# a = 10
+# b = 5
+# c = 0
+# if a < b or not c:
+#     print(a)
+# else:
+#     print(c)
+
+
+"""5"""
+
+# a = 5
+# b = 5
+# c = 3
+# if a > b > c:
+#     if a > b:
+#         print(a)
+#     else:
+#         print(b)
+# else:
+#     if b > c:
+#         print(c)
