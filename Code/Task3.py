99d6d04b3f5113b09c53b01ae3d381c3af864c64
Python <python@it.step>
2023-05-27 20:55:44 +0300

    Added tasks for Topic 9

--- Code/Task3.py
+++ Code/Task3.py
@@ -1,5 +1,19 @@
 """
 Дано целое положительное число N. Вывести все числа Фибоначчи до числа, большего N.
 Вывод чисел запиши через пробел.
+a = 2
+b = 3
+
+a, b = b, a
+print(a, b)
 """
 
+n = int(input())
+
+fib1, fib2 = 1, 1
+
+print(fib1, fib2, end=" ")
+
+for i in range(n - 1):
+    fib2, fib1 = fib1 + fib2, fib2
+    print(fib2, end=" ")
--- Code/Task8.py
+++ Code/Task8.py
@@ -1,4 +1,17 @@
 """
 На вход программе поступает список из целых чисел. Ваша задача найти в данном списке наименьшее положительное значение.
 В случае, если положительных значений нет, выведите строку "Empty"
+[5, 4, -3, 0, -15]
 """
+
+nums = list(map(int, input().split()))
+min = nums[0]
+
+for elem in nums:
+    if elem < 0 and elem == 0:
+        continue
+    if elem > 0:
+        if min > elem:
+            min = elem
+
+print("Empty" if min < 0 else min)
