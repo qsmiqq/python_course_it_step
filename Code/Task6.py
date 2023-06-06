bd612be111a39a27afacc84a19642173776a1b34
Aliaksandr Dvornik <aadvornik@gmail.com>
2023-06-06 17:11:33 +0300

    Updated

--- Code/Task6.py
+++ Code/Task6.py
@@ -3,15 +3,3 @@ ROT13 — это простой шифр, который заменяет каж
 которая находится на 13 букв после неё в алфавите. Программа получает на вход строку,
 необходимо зашифровать её с помощью ROT13 и вывести результат.
 """
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-n = 13
-text = input()
-result = ''
-
-for item in text:
-    if item == ' ':
-        result += ' '
-    else:
-        new_index = (alphabet.find(item) + n) % len(alphabet)
-        result += alphabet[new_index]
-print('Result: "' + result + '"', end='')
