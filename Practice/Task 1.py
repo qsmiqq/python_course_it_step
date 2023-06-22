7d9d48076f9ad94cb4deb9fdb60565106fb1ff42
Aliaksandr Dvornik <aadvornik@gmail.com>
2023-06-22 17:51:19 +0300

    New tasks for practice

---
+++ Practice/Task 1.py
@@ -0,0 +1,15 @@
+"""
+Имеется список words
+Задача создать список кортежей words_with_position, где каждый элемент-кортеж должен содержать два значения:
+само слово и его порядковый номер в списке words
+Порядковый номер слов необходимо считать с единицы.
+
+
+words = ['variation', 'random', 'electronics', 'competence', 'collapse']
+
+words_with_position = [('variation', 1),
+                       ('random', 2),
+                       ('electronics', 3),
+                       ('competence', 4),
+                       ('collapse', 5)]
+"""
---
+++ Practice/Task 9.py
@@ -0,0 +1,13 @@
+"""
+Сколько стоят все товары из словаря supermarket
+"""
+
+supermarket = {
+    "milk": {"quantity": 20, "price": 1.19},
+    "biscuits": {"quantity": 32, "price": 1.45},
+    "butter": {"quantity": 20, "price": 2.29},
+    "cheese": {"quantity": 15, "price": 1.90},
+    "bread": {"quantity": 15, "price": 2.59},
+    "cookies": {"quantity": 20, "price": 4.99}
+}
+
