b0d4e87193be2142e64f70247e2bd109fd6fb53a
ADv0rnik <aadvornik@gmail.com>
2023-09-07 17:28:15 +0300

    Created new tasks

---
+++ Code/example2_SOLID.py
@@ -0,0 +1,9 @@
+"""S - single responsibility"""
+
+"""O - opening"""
+
+"""L - Liskov principle"""
+
+"""I - Interface separated principle разделение интерфейсов (ISP)"""
+
+"""D - DIP инверсия зависимостей"""
